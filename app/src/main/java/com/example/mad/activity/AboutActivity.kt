a35abc14688cb31a36ba8941756d2cb3f12a4789
IT21271250 <IT21271250@my.sliit.lk>
2023-05-11 06:44:33 +0530

    initial commit

--- app/src/main/java/com/example/mad/activity/AboutActivity.kt
+++ app/src/main/java/com/example/mad/activity/AboutActivity.kt
@@ -1,5 +1,12 @@
 package com.example.mad.activity
 
-class AboutActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.mad.R
 
+class AboutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.about)
+    }
 }
